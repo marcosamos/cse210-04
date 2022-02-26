4f5aeb0251a35ba57c80549f253c283c2277da7a
marcosamos <ucs21001@byui.edu>
2022-02-25 21:01:40 -0800

    add code in Gem

--- greed/game/casting/gem.py
+++ greed/game/casting/gem.py
@@ -2,6 +2,14 @@
 
 from game.casting.actor import Actor
 
+FRAME_RATE = 12
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
+ROWS = 40
+
 """ Class Gem is passing the argument actor like a intance of Class actor """
 class Gem(Actor):
     
