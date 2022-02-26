f5c66c2cb45d7c3ca8b8e22a67ff3608662c420e
marcosamos <ucs21001@byui.edu>
2022-02-25 20:57:57 -0800

    add the code for gem class

--- greed/game/casting/gem.py
+++ greed/game/casting/gem.py
@@ -1,37 +1,10 @@
-# ### used to provide message over artifacts - 
 # will be used to add up points from gem or rocks
-
-
-
 
 from game.casting.actor import Actor
 
-
-class Artifact(Actor):
-    """
-    An item of cultural or historical interest. 
+""" Class Gem is passing the argument actor like a intance of Class actor """
+class Gem(Actor):
     
-    The responsibility of an Artifact is to provide a message about itself.
-
-    Attributes:
-        _message (string): A short description about the artifact.
-    """
     def __init__(self):
         super().__init__()
-        self._pointplus = None
-        
-    def get_message(self):
-        """Gets the artifact's message.
-        
-        Returns:
-            string: The message.
-        """
-        return self._pointplus
-    
-    def set_message(self, pointplus):
-        """Updates the message to the given one.
-        
-        Args:
-            message (string): The given message.
-        """
-        self._point = pointplus
+        pass
